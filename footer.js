f6173f346ed804079ed5536f2eeade4712d84cc0
JulietaHRodriguez <131625971+JulietaHRodriguez@users.noreply.github.com>
2023-05-02 01:00:11 -0300

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,16 @@
+document.open();
+document.write("<div class='redes'>");
+document.write("<div class='info'>");
+document.write("    <p>Viajes Auricular Argentina S.A.</p>");
+document.write("        <p> Domicilio legal Santa Fe 100 - CABA </p>");
+document.write("        <p>Telefono de atención Tel: 0800-AURI-1234</p>");
+    
+document.write("</div>");
+document.write("<div>");
+document.write("    <p>Seguinos</p>");
+document.write("    <a href='https://www.facebook.com'><img src='Images/facebook.PNG' alt=''></a>");
+document.write("    <a href='https://www.instagram.com/'><img src='Images/instagram.PNG' alt=''></a>");
+document.write("    <a href='https://www.tiktok.com'><img src='Images/tiktok.PNG' alt=''></a>");
+document.write("</div>");
+document.write("</div>");
+document.close();
